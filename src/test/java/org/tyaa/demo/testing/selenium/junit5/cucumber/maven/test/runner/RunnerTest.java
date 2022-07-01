826e3b09cf9cb6ecd1703e59d7fc45e5c104b8ac
YuriiTrofimenko <tyaa@ukr.net>
2022-07-01 15:08:54 +0300

    fix generate allure results

--- src/test/java/org/tyaa/demo/testing/selenium/junit5/cucumber/maven/test/runner/RunnerTest.java
+++ src/test/java/org/tyaa/demo/testing/selenium/junit5/cucumber/maven/test/runner/RunnerTest.java
@@ -1,16 +1,16 @@
 package org.tyaa.demo.testing.selenium.junit5.cucumber.maven.test.runner;
 
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+/* import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME; */
 
-@Suite
+/* @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("src/main/resources/smoke.feature")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.tyaa.demo.testing.selenium.junit5.cucumber.maven.steps.SmokeSteps") */
 public class RunnerTest {
 }
